edee5f05bf70a9f7f89f2154c39d51c69a44b91f
Chris Eustis <cjeustis@gmail.com>
2016-09-05 17:43:33 -0400

    Get rid of unwanted files. minor fix in styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
 import {Component, OnInit} from "@angular/core";
 import {RouteConfig, RouterLink, ROUTER_DIRECTIVES} from "@angular/router-deprecated";
 
-import {TaskListComponent} from "./todo/components/task-list.component";
-import {AboutComponent} from "./about/components/about.components";
 import {DashboardComponent} from "./dashboard/dashboard.component";
 import {SignupComponent} from "./signUp/signup.component";
 import {SigninComponent} from "./signIn/signin.component";
@@ -15,12 +13,10 @@ declare var __moduleName: string;
   moduleId: __moduleName,
   selector: "app",
   templateUrl: "./app.html",
-  directives: [TaskListComponent, AboutComponent, SignupComponent, SigninComponent, RouterLink, ROUTER_DIRECTIVES]
+  directives: [SignupComponent, SigninComponent, RouterLink, ROUTER_DIRECTIVES]
 })
 @RouteConfig([
   {path: '/', component: DashboardComponent, as: 'Home', useAsDefault: true},
-  {path: '/tasks', component: TaskListComponent, as: 'TaskList'},
-  {path: '/about', component: AboutComponent, as: 'About'},
   {path: '/signin', component: SigninComponent, as: 'SignIn'},
   {path: '/signup', component: SignupComponent, as: 'SignUp'}
 ])
