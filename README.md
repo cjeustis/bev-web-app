99f3954709ce4900fabd3a692d754df45f388736
Chris Eustis <ceustis@penbaysolutions.com>
2016-09-05 13:19:15 -0400

    Initial project startup - typescript/angular2/gulp

---
+++ README.md
@@ -0,0 +1,26 @@
+Angular2 with TypeScript and Gulp
+=================================
+
+Prerequisites
+-------------
+
+- nodejs
+- gulp and gulp-cli
+- typings
+- typescrit
+- ts-node
+
+Running
+-------
+
+Install dependencies:
+
+> npm install
+
+Build the project:
+
+> npm run clean & npm run build
+
+> npm start
+
+The application should be displayed in the browser.
---
+++ gulpfile.ts
@@ -0,0 +1,91 @@
+"use strict";
+
+const gulp = require("gulp");
+const del = require("del");
+const tsc = require("gulp-typescript");
+const sourcemaps = require('gulp-sourcemaps');
+const tsProject = tsc.createProject("tsconfig.json");
+const tslint = require('gulp-tslint');
+const util = require("gulp-util")
+const sass = require("gulp-sass")
+const autoprefixer = require("gulp-autoprefixer")
+const minifycss = require("gulp-minify-css")
+const rename = require("gulp-rename")
+const concat = require("gulp-concat")
+
+
+// Clean dist folder
+gulp.task('clean', (cb) => {
+    return del(["build"], cb);
+});
+
+
+// Compile Sass into css
+gulp.task('sass', () => {
+    return gulp.src("src/**/*.scss")
+        .pipe(sass().on('error', sass.logError))
+        .pipe(concat('main.css'))
+        .pipe(sass({outputStyle: 'expanded'}))
+        .pipe(gulp.dest('build/css'));
+});
+
+
+// Link up ts files
+gulp.task('tslint', () => {
+    return gulp.src("src/**/*.ts")
+        .pipe(tslint())
+        .pipe(tslint.report('prose'));
+});
+
+
+// Compile ts and create sourcemaps
+gulp.task("compile", ["tslint"], () => {
+    let tsResult = gulp.src("src/**/*.ts")
+        .pipe(sourcemaps.init())
+        .pipe(tsc(tsProject));
+    return tsResult.js
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest("build"));
+});
+
+
+// Copy all other files
+gulp.task("resources", () => {
+    return gulp.src(["src/**/*", "!**/*.ts", "!**/*.scss"])
+        .pipe(gulp.dest("build"));
+});
+
+
+// Copy libraries we need
+gulp.task("libs", () => {
+    return gulp.src([
+            'es6-shim/es6-shim.min.js',
+            'systemjs/dist/system-polyfills.js',
+            'systemjs/dist/system.src.js',
+            'reflect-metadata/Reflect.js',
+            'rxjs/**',
+            'zone.js/dist/**',
+            '@angular/**'
+        ], {cwd: "node_modules/**"}) /* Glob required here. */
+        .pipe(gulp.dest("build/lib"));
+});
+
+
+// Watch for changes
+gulp.task('watch', () => {
+    gulp.watch(["src/**/*.ts"], ['compile']).on('change', function (e) {
+        console.log('TypeScript file ' + e.path + ' has been changed. Compiling.');
+    });
+    gulp.watch(["src/**/*.html"], ['resources']).on('change', function (e) {
+        console.log('Resource file ' + e.path + ' has been changed. Updating.');
+    });
+    gulp.watch(["src/**/*.scss"], ['sass']).on('change', function (e) {
+        console.log('Style file ' + e.path + ' has changed. Compiling.');
+    });
+});
+
+
+// Build out the project
+gulp.task("build", ['sass', 'compile', 'resources', 'libs'], () => {
+    console.log("Building the project ...");
+});
